1d30a5973d9e5e89fc00994e9a5cac06c76998b7
Daniel Shwan <dannyshwan@gmail.com>
2018-12-21 23:16:34 -0500

    New problem solved

---
+++ CrackingTheCode/Q1a.py
@@ -0,0 +1,18 @@
+'''
+Checks to see if all characters in a string are unique
+'''
+
+def isUnique(str):
+    charArray = list(str)
+    charArray.sort
+
+    for i in range(0,len(charArray)):
+        if(((i+1) != len(charArray)) and (charArray[i] == charArray[i+1])):
+            return False
+    return True
+
+string1 = "abcdefg"
+string2 = "abcdefg1123"
+
+print(isUnique(string1)) #Should print True
+print(isUnique(string2)) #Shoud print False
