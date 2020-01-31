f6eb93fa20cf7369ac89f0eefb569c08d79ae945
dannyshwan <dannyshwan@gmail.com>
2020-01-30 22:55:08 -0500

    new problem solved

---
+++ LeetCode/Python/RotateMatrix.py
@@ -0,0 +1,15 @@
+class Solution:
+   def rotate(self, matrix: List[List[int]]) -> None:
+      """
+      Do not return anything, modify matrix in-place instead.
+      """
+      n = len(matrix)
+      
+      #transpose
+      for i in range(n):
+         for j in range(i+1,n):
+            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+      
+      #reverse
+      for row in matrix:
+          ow.reverse()
